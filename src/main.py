ca9a6bfa212397db119495fc8af3179c892c7665
Dima <dima290012007@yandex.ru>
2025-07-16 19:23:59 +0300

    fix enter config values

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import back
 import config
+from enum import Enum
 
 
 def check_int_input(message):
@@ -11,31 +12,39 @@ def check_int_input(message):
 			return int(arg)
 
 
+def enter_keys():
+	public_key = input('Вы не ввели api ключ или ввели неверный. Введите другой public key: ')
+	secret_key = input('Secret key: ')
+	return public_key, secret_key
+
+
 def main():
+	Modes = Enum('Modes', [('SPOT', 1), ('DEMO', 2)])
+	Lenghts = Enum('Lenghts', [('PUBLIC_LEN', 18), ('SECRET_LEN', 36)])
+
 	input('''Привет, перед началом использования бота заполни пожалуйста файл config,
 ключи api для demo-режима необязательны, если не хочешь использовать его.
 Если всё готово, нажми Enter.''')
-	mode = check_int_input('Выберите режим торговли spot(1)/demo(2), напиши цифру. ')
+
+	mode = check_int_input('Выберите режим торговли spot(1)/demo(2), напишите цифру. ')
 	while True:
-		if mode == 1:
-			if len(config.API_public) == 18 and len(config.API_secret) == 36:
-				api_public = config.API_public
-				api_secret = config.API_secret
+		if mode == Modes['SPOT'].value:
+			if len(config.api_public) == Lenghts['PUBLIC_LEN'].value\
+					and len(config.api_secret) == Lenghts['SECRET_LEN'].value:
 				break
 			else:
-				input('Вы не ввели api ключ или ввели неверный. Введите другой.')
-		else:
-			if len(config.demo_API_public) == 18 and len(config.demo_API_secret) == 36:
-				api_public = config.demo_API_public
-				api_secret = config.demo_API_secret
+				config.API_public, config.API_secret = enter_keys()
+		elif mode == Modes['DEMO'].value:
+			if len(config.demo_api_public) == Lenghts['PUBLIC_LEN'].value\
+					and len(config.demo_api_secret) == Lenghts['SECRET_LEN'].value:
 				break
 			else:
-				input('Вы не ввели api ключ или ввели неверный. Введите другой.')
+				config.demo_API_public, config.demo_API_secret = enter_keys()
 
 	token = input('Введите тикер ').upper() + 'USDT'
 	order_volume = check_int_input('Объем одного ордера в $ ')
 
-	trader = back.Trade(api_public, api_secret, token, order_volume)
+	trader = back.Trade(config.api_public, config.api_secret, token, order_volume)
 
 
 if __name__ == '__main__':
