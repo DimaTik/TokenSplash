9b9d71f4338a083f14d32ed87e93375f48d15c8e
Dima <dima290012007@yandex.ru>
2025-07-19 16:59:18 +0300

    protection against incorrect name

--- src/back.py
+++ src/back.py
@@ -1,6 +1,7 @@
 import random as rd
 import time
 from pybit.unified_trading import HTTP
+from pybit import exceptions
 
 
 class Trade:
@@ -19,47 +20,38 @@ class Trade:
 		self.PRICE_DEVIATION = 0.0001
 
 	def __make_order(self):
-		price = self.__get_price_of_token()
-		price_precision = len(str(price).split('.')[-1])
-		base_precision = self.__get_base_precision()
-		price = round(price * (1 - self.PRICE_DEVIATION), price_precision)
-		take_profit = str(round(price * (1+self.PERCENT_OF_PROFIT), price_precision))
-		stop_loss = str(round(price * (1-self.PERCENT_OF_LOSS), price_precision))
-		volume = rd.randint(int(self.order_volume*(1-0.1)), int(self.order_volume*(1+0.1)))
-		qty = round(volume / price, base_precision)
-		req = self.session.place_order(
-			category="spot",
-			symbol=self.token,
-			side="Buy",
-			orderType="Limit",
-			price=price,
-			marketUnit="baseCoin",
-			qty=qty,
-			isLeverage=0,
-			orderFilter="Order",
-			positionIdx=0,
-			tpslMode='Partial',
-			tpOrderType='Limit',
-			slOrderType='Limit',
-			tpLimitPrice=take_profit,
-			slLimitPrice=stop_loss)
-		print('make_order')
-		print(req)
-		return volume, req['result']['orderId']
-
-	# def __close_order(self, orderId):
-	# 	self.session.place_order(
-	# 		category="linear",
-	# 		symbol=self.token,
-	# 		side="Sell",
-	# 		orderType="Limit",
-	# 		marketUnit="quoteCoin",
-	# 		qty=self.order_volume,
-	# 		timeInForce="PostOnly",
-	# 		isLeverage=0,
-	# 		orderFilter="Order",
-	# 		positionIdx=0
-	# 	)
+		while True:
+			try:
+				price = self.__get_price_of_token()
+				price_precision = len(str(price).split('.')[-1])
+				base_precision = self.__get_base_precision()
+				price = round(price * (1 - self.PRICE_DEVIATION), price_precision)
+				take_profit = str(round(price * (1+self.PERCENT_OF_PROFIT), price_precision))
+				stop_loss = str(round(price * (1-self.PERCENT_OF_LOSS), price_precision))
+				volume = rd.randint(int(self.order_volume*(1-0.1)), int(self.order_volume*(1+0.1)))
+				qty = round(volume / price, base_precision)
+				req = self.session.place_order(
+					category="spot",
+					symbol=self.token,
+					side="Buy",
+					orderType="Limit",
+					price=price,
+					marketUnit="baseCoin",
+					qty=qty,
+					isLeverage=0,
+					orderFilter="Order",
+					positionIdx=0,
+					tpslMode='Partial',
+					tpOrderType='Limit',
+					slOrderType='Limit',
+					tpLimitPrice=take_profit,
+					slLimitPrice=stop_loss)
+				print('make_order')
+				print(req)
+				return volume, req['result']['orderId']
+			except exceptions.InvalidRequestError:
+				print('Вы ввели несуществующий тикер')
+				self.token = input('Введите новый ').upper() + 'USDT'
 
 	def __get_base_precision(self):
 		return len(str(self.session.get_instruments_info(
@@ -109,6 +101,5 @@ class Trade:
 				continue
 			else:
 				self.__wait_close_position()
-			print('OK')
-
-
+				print('OK')
+			time.sleep(rd.randint(10, 30))
